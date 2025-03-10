4305fa4a04f40fd59113ed628c78ba3827f1d080
im <ilia@mikhailov.se>
2025-03-10 10:47:21 +0100

    style: Reformat code and remove duplicate setupRoutes function

--- cmd/srun/main.go
+++ cmd/srun/main.go
@@ -1,39 +1,32 @@
 package main
 
 import (
-    "log"
-    "github.com/gin-gonic/gin"
-    "srun/internal/core"
+	"log"
+	"srun/internal/core"
+
+	"github.com/gin-gonic/gin"
 )
 
 func setupRoutes(r *gin.Engine, pm *core.ProcessManager) {
-    // Example route setup
-    r.POST("/jobs", func(c *gin.Context) {
-        // Implementation here
-    })
+	// Example route setup
+	r.POST("/jobs", func(c *gin.Context) {
+		// Implementation here
+	})
 }
 
 func main() {
-    store, err := core.NewSQLiteStorage("srun.db")
-    if err != nil {
-        log.Fatal(err)
-    }
+	store, err := core.NewSQLiteStorage("srun.db")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    pm := &core.ProcessManager{
-        Jobs:    make(map[string]*core.Job),
-        Store:   store,
-        LogChan: make(chan core.LogMessage, 1000),
-    }
+	pm := &core.ProcessManager{
+		Jobs:    make(map[string]*core.Job),
+		Store:   store,
+		LogChan: make(chan core.LogMessage, 1000),
+	}
 
-    r := gin.Default()
-    setupRoutes(r, pm)
-
-    r.Run(":8080")
+	r := gin.Default()
+	setupRoutes(r, pm)
+	r.Run(":8080")
 }
-
-func setupRoutes(r *gin.Engine, pm *core.ProcessManager) {
-     // Example route setup
-     r.POST("/jobs", func(c *gin.Context) {
-         // Implementation here
-     })
- }
